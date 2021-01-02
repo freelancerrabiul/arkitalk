832bcd24625072e8e0c50e03d86a8cd99bc1951c
polash <johndoe@example.com>
2021-01-02 16:55:09 +0600

    did clean up process by deleting some files and add firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBCf1HUM6JQ54gFqVOxwykZS3NzbbSC_xU",
+  authDomain: "arkitalk.firebaseapp.com",
+  projectId: "arkitalk",
+  storageBucket: "arkitalk.appspot.com",
+  messagingSenderId: "776774778369",
+  appId: "1:776774778369:web:e6872ddd16a6b8d8954a00",
+  measurementId: "G-2G8KW7V26N"
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
